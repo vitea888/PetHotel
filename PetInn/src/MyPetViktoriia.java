93c6cac8c1f1f59ec0f7a13881145f29048c693a
viktoriiakhudiiva <vika.yani89@gmail.com>
2025-10-03 13:42:20 -0400

    changes1

--- PetInn/src/MyPetViktoriia.java
+++ PetInn/src/MyPetViktoriia.java
@@ -3,7 +3,7 @@ public class MyPetViktoriia {
     public static void main(String[] args) {
 
 
-        public class MyPetViktoriia() {
+        public String MyPetViktoriia() {
             public String petName = "Buddy";
             public String ownerName = "Viktoriia";
         }
