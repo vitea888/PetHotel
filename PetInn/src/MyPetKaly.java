ab69170dcaba62706141619f4b3226171f6b5c9d
munozkaly <munozkaly@yahoo.com>
2025-10-03 13:41:18 -0400

    Added my pet

---
+++ PetInn/src/MyPetKaly.java
@@ -0,0 +1,13 @@
+public class MyPetKaly {
+    public static void main(String[] args) {
+       String petName = "Buddy";
+       String ownerName = "Alice";
+
+        System.out.println(ownerName + " Checked in " + petName);
+
+    }
+
+}
+
+
+
