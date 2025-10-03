20c1e667cdd74d0871598c93bdff4ce3ee4b1f3f
mihajlo3492 <mihajlompetkovic@gmail.com>
2025-10-03 14:15:52 -0400

    Update MyPetMihajlo.java

--- PetInn/src/MyPetMihajlo.java
+++ PetInn/src/MyPetMihajlo.java
@@ -5,6 +5,7 @@ public class MyPetMihajlo {
         String petName = "Norton";
         String ownerName = "Mihajlo";
         int age = 5;
+        char gender = 'M';
 
         System.out.println(ownerName + " checked in their pet: " + petName);
 
