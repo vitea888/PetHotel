b88b426cbcd0bfae4f3d7781ffc0f082537d0569
Sasa <glidza@gmail.com>
2025-10-03 13:41:07 -0400

    My Pet

---
+++ PetInn/src/MyPetGas.java
@@ -0,0 +1,9 @@
+public class MyPetGas {
+    public static void main(String[] args) {
+
+        String petName = "Gas";
+        String ownerName = "Sasa";
+
+        System.out.println(petName + " check in " + ownerName);
+    }
+}
