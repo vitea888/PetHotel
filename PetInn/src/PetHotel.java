743fcd1b74da4c623d881f7a9ee1a882f9eeda97
vitea888 <vitea88@gmail.com>
2025-10-03 10:56:10 -0500

    Pet Hotel class PetHotel Client class

---
+++ PetInn/src/PetHotel.java
@@ -0,0 +1,7 @@
+public class PetHotel {
+
+
+    public void checkIn(String petName, String ownerName) {
+        System.out.println(ownerName + " checked in their pet: " + petName);
+    }
+}
---
+++ PetInn/src/PetHotelClients.java
@@ -0,0 +1,13 @@
+public class PetHotelClients {
+
+    public static void main(String[] args) {
+        PetHotel hotel = new PetHotel();
+
+        //new MyPetAlice().checkIn(hotel);
+        //new MyPetBob().checkIn(hotel);
+        //new MyPetCharlie().checkIn(hotel);
+        //new MyPetDiana().checkIn(hotel);
+        //new MyPetEve().checkIn(hotel);
+        //new MyPetFrank().checkIn(hotel);
+    }
+}
