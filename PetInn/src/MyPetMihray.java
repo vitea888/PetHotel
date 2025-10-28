1ea69d9409889d4fa32a76e7841a927895fe129a
mihra <mihray0126@gmail.com>
2025-10-28 15:14:21 -0400

    Mihray's pet class added

---
+++ PetInn/src/MyPetMihray.java
@@ -0,0 +1,11 @@
+public class MyPetMihray {
+    public static void main(String[] args) {
+
+        String petName = "Quandak";
+        String ownerName = "Mihray";
+
+        System.out.println(ownerName +" checked in "+petName);
+    }
+
+
+}
